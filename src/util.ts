bffad0ca1af91a0a8e95f1bcae0dda40efe1bf64
Pon <perterpon@gmail.com>
2019-08-13 17:35:08 +0800

    feat(typescript): update typescript version

--- src/util.ts
+++ src/util.ts
@@ -5,7 +5,7 @@
  * Create: Tue Aug 13 2019 16:41:00 GMT+0800 (中国标准时间)
  */
 
-export async function sleep(time: number): Promise<void> {
+export async function sleep(time: number): Promise<unknown> {
     return new Promise((resolve, reject) => {
         setTimeout(resolve, time);
     });
